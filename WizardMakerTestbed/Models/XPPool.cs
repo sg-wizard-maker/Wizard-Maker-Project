a17472758e82ddd5f3bc859d2f711f6cd1732c50
LeeTL1220 <LeeTL1220@hotmail.com>
2022-07-27 22:36:58 -0400

    Maintenance commit.

---
+++ WizardMakerTestbed/Models/XPPool.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo("WizardMakerTests")]
+namespace WizardMakerPrototype.Models
+{
+    /*
+     * This class expects the caller to manage the XP expenditures in a pool and make sure that it is not 
+     *  overdrawn.
+     */
+    public class XPPool
+    {
+        public string name { get; }
+        public string description { get; }
+        private int initialXP;
+        public int remainingXP { get; set; }
+
+        public XPPool(string name, string description, int initialXP)
+        {
+            this.name = name;
+            this.description = description;
+            this.initialXP = initialXP;
+            this.remainingXP = initialXP;
+        }
+
+        public void spendXP(int xp)
+        {
+            if (this.remainingXP < xp)
+            {
+                throw new XPPoolOverdrawnException("XP Pool " + name + " asked for too many XP: " + xp + " > " + this.remainingXP);
+            }
+            this.remainingXP -= xp;
+        } 
+    }
+
+}
