706761d0286a226c8fc569ac9d241c293fdae4dd
LeeTL1220 <LeeTL1220@hotmail.com>
2022-09-10 20:29:26 -0400

    Filled out most of the Warrior Simple Test

--- WizardMakerTestbed/Models/Virtues/VirtueCommands/WarriorCommand.cs
+++ WizardMakerTestbed/Models/Virtues/VirtueCommands/WarriorCommand.cs
@@ -8,13 +8,16 @@ namespace WizardMakerPrototype.Models.Virtues.VirtueCommands
 {
     public class WarriorCommand : ICharacterCommand
     {
+        public static string WARRIOR_POOL_NAME = ArchVirtue.NameToArchVirtue["Warrior"].Name;
+        public static string WARRIOR_POOL_DESCRIPTION = "XP Pool for Virtue Warrior";
+        public static int WARRIOR_POOL_INITIAL_XP = 50;
         public void Execute(Character c)
         {
             // Allow access to Martial skills
             c.AllowedAbilityTypes.Add(AbilityType.Martial);
 
             // Create a martial 50XP pool
-            c.XPPoolList.Add(new CategoryAbilityXpPool(ArchVirtue.NameToArchVirtue["Warrior"].Name, "XP Pool for Virtue Warrior", 50,
+            c.XPPoolList.Add(new CategoryAbilityXpPool(WARRIOR_POOL_NAME, WARRIOR_POOL_DESCRIPTION, WARRIOR_POOL_INITIAL_XP,
                 new List<AbilityType>() { AbilityType.Martial }));
         }
 
--- WizardMakerTests/Models/Virtues/VirtueCommands/WarriorCommandTests.cs
+++ WizardMakerTests/Models/Virtues/VirtueCommands/WarriorCommandTests.cs
@@ -26,13 +26,26 @@ namespace WizardMakerPrototype.Models.Virtues.VirtueCommands.Tests
             c.addJournalable(virtueJournalEntry);
             CharacterRenderer.RenderAllJournalEntries(c);
 
-            // TODO: Assert that the Warrior pool is there.
-            // TODO: Assert that the Warrior pool has 50XP.
-            // TODO: Assert that the AbilityType.Martial is allowed
+            // Assert that the XP Pool was created and has the correct amount of XP (initial and remaining)
+            Assert.IsTrue(c.XPPoolList.Where(x => x.name.Equals(WarriorCommand.WARRIOR_POOL_NAME)).Any());
+            Assert.AreEqual(WarriorCommand.WARRIOR_POOL_INITIAL_XP, c.XPPoolList.Where(x => x.name.Equals(WarriorCommand.WARRIOR_POOL_NAME)).First().remainingXP);
+            Assert.AreEqual(WarriorCommand.WARRIOR_POOL_INITIAL_XP, c.XPPoolList.Where(x => x.name.Equals(WarriorCommand.WARRIOR_POOL_NAME)).First().initialXP);
+
+            // Assert that the AbilityType.Martial is allowed
+            Assert.IsTrue(c.AllowedAbilityTypes.Where(a => a == AbilityType.Martial).Any());
+
             // TODO: Assert that there were no validation errors
 
-            // TODO: Add a martial ability and rerender the character.
-            // TODO: Assert that the Warrior XP Pool has been debited appropriately
+            // Add a martial ability and rerender the character.
+            XpAbilitySpendJournalEntry xpSpend = new XpAbilitySpendJournalEntry("Test XP Spend", new SeasonYear(SAGA_START - STARTING_AGE, Season.SPRING),
+                "Great Weapon", 25, "Poleaxe");
+            c.addJournalable(xpSpend);
+            CharacterRenderer.RenderAllJournalEntries(c);
+
+            // Assert that the Warrior XP Pool has been debited appropriately
+            Assert.AreEqual(WarriorCommand.WARRIOR_POOL_INITIAL_XP-25, c.XPPoolList.Where(x => x.name.Equals(WarriorCommand.WARRIOR_POOL_NAME)).First().remainingXP);
+            Assert.AreEqual(WarriorCommand.WARRIOR_POOL_INITIAL_XP, c.XPPoolList.Where(x => x.name.Equals(WarriorCommand.WARRIOR_POOL_NAME)).First().initialXP);
+
             // TODO: Assert that there were no validation errors
         }
     }
