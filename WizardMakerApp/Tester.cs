2c7dd7ca85a377279c0375e0b42508d430ed3132
Sam Glasby <sam.glasby@gmail.com>
2023-04-20 01:29:50 -0600

    File-scoped namespaces for the Application source files

--- WizardMakerApp/Tester.cs
+++ WizardMakerApp/Tester.cs
@@ -1,151 +1,143 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+
 using WizardMaker.DataDomain.Models;
-using Newtonsoft.Json;
 
-namespace WizardMaker
+namespace WizardMaker;
+
+public partial class Tester : Form
 {
-    public partial class Tester : Form
-    {
-        public CharacterManager characterManager;
-        
+    public CharacterManager characterManager;
+    
 
-        public Tester()
+    public Tester()
+    {
+        InitializeComponent();
+    }
+
+    private void Tester_Load(object sender, EventArgs e)
+    {
+        //TODO: Magic number 25.  Make this specifiable by user
+        this.characterManager = new CharacterManager(25, ArchAbility.LangEnglish);
+        this.Text = characterManager.GetCharacterName();
+        updateCharacterDisplay();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        AbilityDialog abilityDialog = new AbilityDialog();
+
+        // Show testDialog as a modal dialog and determine if DialogResult = OK.
+        if (abilityDialog.ShowDialog(this) == DialogResult.OK)
         {
-            InitializeComponent();
+            // Read the contents of testDialog's TextBox.
+            // TODO: Make the abilityDialog attributes non-public
+            if (abilityDialog.abilityListBoxDialog.SelectedItem != null)
+            {
+                characterManager.UpdateAbilityDuringCreation(abilityDialog.abilityListBoxDialog.SelectedItem.ToString(),
+                    ((int)abilityDialog.xpUpdown1.Value), abilityDialog.specialtyComboBox1.Text);
+                
+                updateCharacterDisplay();
+            }
         }
 
-        private void Tester_Load(object sender, EventArgs e)
+        abilityDialog.Dispose();
+        
+        }
+
+    /**
+     * General update method for updating the display of the character.
+     */
+    private void updateCharacterDisplay()
+    {
+
+        dataGridView1.Rows.Clear();
+
+        CharacterData c = characterManager.RenderCharacterAsCharacterData();
+
+        foreach (var a in c.Abilities)
         {
-            //TODO: Magic number 25.  Make this specifiable by user
-            this.characterManager = new CharacterManager(25, ArchAbility.LangEnglish);
-            this.Text = characterManager.GetCharacterName();
+            //TODO: Make constants from the magic numbers
+            dataGridView1.Rows.Add(a.Name);
+            dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[2].Value = a.XP.ToString();
+            dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[1].Value = a.Score.ToString();
+            dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[3].Value = a.Specialty;
+        }
+        dataGridView1.Sort(dataGridView1.Columns[0], ListSortDirection.Ascending);
+
+        // Update the XPPools text
+        string xpPoolText = String.Join("\n", c.XPPools.Select(x => x.Name + " (Remaining: " + x.RemainingXP + ")").ToList());
+        XPPoolsJson.Text = xpPoolText;
+    }
+
+    private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+    {
+        int rowIndex = e.RowIndex;
+        int colIndex = e.ColumnIndex;
+
+        // Handle a deletion
+        if (dataGridView1.Columns[colIndex].HeaderText == "Delete")
+        {
+
+            string deletedAbility = dataGridView1.Rows[rowIndex].Cells[0].Value.ToString();
+            List<string> abilityIds = retrieveAbilityIdsFromAbilityName(deletedAbility);
+            if (abilityIds == null)
+            {
+                throw new ShouldNotBeAbleToGetHereException("Could not find ID for " + deletedAbility);
+            }
+            foreach (string abilityId in abilityIds) { 
+                characterManager.DeleteJournalEntry(abilityId);
+            }
             updateCharacterDisplay();
         }
+    }
 
-        private void button1_Click(object sender, EventArgs e)
+    private List<string> retrieveAbilityIdsFromAbilityName(string name)
+    {
+        CharacterData c = characterManager.RenderCharacterAsCharacterData();
+        List<string> result = new List<string>();
+        foreach (var a in c.Abilities)
         {
-            AbilityDialog abilityDialog = new AbilityDialog();
-
-            // Show testDialog as a modal dialog and determine if DialogResult = OK.
-            if (abilityDialog.ShowDialog(this) == DialogResult.OK)
+            if (a.Name == name)
             {
-                // Read the contents of testDialog's TextBox.
-                // TODO: Make the abilityDialog attributes non-public
-                if (abilityDialog.abilityListBoxDialog.SelectedItem != null)
-                {
-                    characterManager.UpdateAbilityDuringCreation(abilityDialog.abilityListBoxDialog.SelectedItem.ToString(),
-                        ((int)abilityDialog.xpUpdown1.Value), abilityDialog.specialtyComboBox1.Text);
-                    
-                    updateCharacterDisplay();
-                }
-            }
-
-            abilityDialog.Dispose();
-            
-            }
-
-        /**
-         * General update method for updating the display of the character.
-         */
-        private void updateCharacterDisplay()
-        {
-
-            dataGridView1.Rows.Clear();
-
-            CharacterData c = characterManager.RenderCharacterAsCharacterData();
-
-            foreach (var a in c.Abilities)
-            {
-                //TODO: Make constants from the magic numbers
-                dataGridView1.Rows.Add(a.Name);
-                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[2].Value = a.XP.ToString();
-                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[1].Value = a.Score.ToString();
-                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[3].Value = a.Specialty;
-            }
-            dataGridView1.Sort(dataGridView1.Columns[0], ListSortDirection.Ascending);
-
-            // Update the XPPools text
-            string xpPoolText = String.Join("\n", c.XPPools.Select(x => x.Name + " (Remaining: " + x.RemainingXP + ")").ToList());
-            XPPoolsJson.Text = xpPoolText;
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-            int rowIndex = e.RowIndex;
-            int colIndex = e.ColumnIndex;
-
-            // Handle a deletion
-            if (dataGridView1.Columns[colIndex].HeaderText == "Delete")
-            {
-
-                string deletedAbility = dataGridView1.Rows[rowIndex].Cells[0].Value.ToString();
-                List<string> abilityIds = retrieveAbilityIdsFromAbilityName(deletedAbility);
-                if (abilityIds == null)
-                {
-                    throw new ShouldNotBeAbleToGetHereException("Could not find ID for " + deletedAbility);
-                }
-                foreach (string abilityId in abilityIds) { 
-                    characterManager.DeleteJournalEntry(abilityId);
-                }
-                updateCharacterDisplay();
+                result.AddRange(a.Id);
             }
         }
 
-        private List<string> retrieveAbilityIdsFromAbilityName(string name)
+        return result;
+    }
+
+    private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+    {
+        int rowIndex = e.RowIndex;
+        int colIndex = e.ColumnIndex;
+
+        string ability = getDataGridAbility(rowIndex);
+        if (isDataGridXPCell(colIndex))
         {
-            CharacterData c = characterManager.RenderCharacterAsCharacterData();
-            List<string> result = new List<string>();
-            foreach (var a in c.Abilities)
-            {
-                if (a.Name == name)
-                {
-                    result.AddRange(a.Id);
-                }
-            }
-
-            return result;
+            characterManager.UpdateAbilityDuringCreation(ability, int.Parse(getCellValueAsString(rowIndex, colIndex)), getCellValueAsString(rowIndex, 3));
+            updateCharacterDisplay();
         }
+    }
 
-        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+    private string getCellValueAsString(int rowIndex, int colIndex)
+    {
+        if (dataGridView1.Rows[rowIndex].Cells[colIndex].Value != null)
         {
-            int rowIndex = e.RowIndex;
-            int colIndex = e.ColumnIndex;
-
-            string ability = getDataGridAbility(rowIndex);
-            if (isDataGridXPCell(colIndex))
-            {
-                characterManager.UpdateAbilityDuringCreation(ability, int.Parse(getCellValueAsString(rowIndex, colIndex)), getCellValueAsString(rowIndex, 3));
-                updateCharacterDisplay();
-            }
-        }
-
-        private string getCellValueAsString(int rowIndex, int colIndex)
+            return dataGridView1.Rows[rowIndex].Cells[colIndex].Value.ToString();
+        } else
         {
-            if (dataGridView1.Rows[rowIndex].Cells[colIndex].Value != null)
-            {
-                return dataGridView1.Rows[rowIndex].Cells[colIndex].Value.ToString();
-            } else
-            {
-                return "";
-            }
+            return "";
         }
+    }
 
-        private bool isDataGridXPCell(int colIndex)
-        {
-            return (dataGridView1.Columns[colIndex].HeaderText == "XP");
-        }
+    private bool isDataGridXPCell(int colIndex)
+    {
+        return (dataGridView1.Columns[colIndex].HeaderText == "XP");
+    }
 
-        private string getDataGridAbility(int rowIndex)
-        {
-            return dataGridView1.Rows[rowIndex].Cells[0].Value.ToString();
-        }
+    private string getDataGridAbility(int rowIndex)
+    {
+        return dataGridView1.Rows[rowIndex].Cells[0].Value.ToString();
     }
 }
